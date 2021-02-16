991ee413a4ef219ef623f9f96fd541e64923fe27
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-16 20:49:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import requests
+MY_LAT = 23.064740
+MY_LONG = 70.129860
+# response = requests.get(url="http://api.open-notify.org/iss-now.json#")
+#
+# response.raise_for_status()
+#
+# data = response.json()
+#
+#
+# longitude=data["iss_position"]["longitude"]
+# latitude= data["iss_position"]["latitude"]
+#
+# iss_position = (longitude,latitude)
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json#")
+parameters = {
+    "lat": MY_LAT
+    "long": MY_LONG
 
+}
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
-data = response.json()
-
-
-longitude=data["iss_position"]["longitude"]
-latitude= data["iss_position"]["latitude"]
-
-iss_position = (longitude,latitude)
